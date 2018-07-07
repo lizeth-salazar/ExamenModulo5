1cbe45184f55bdc0847abed368f4f3d5d3d0b5b6
liz <liz_salazars@hotmail.com>
2018-07-06 21:34:53 -0400

    Proyecto Generado

---
+++ ExamenProyectoWeb/src/main/java/com/example/ExamenProyectoWeb/ExamenProyectoWebApplication.java
@@ -0,0 +1,12 @@
+package com.example.ExamenProyectoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenProyectoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenProyectoWebApplication.class, args);
+	}
+}
